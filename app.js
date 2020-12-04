face5b11747cfb34c4bdfaf17bfec563def5d4e7
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-12-04 11:23:02 +0100

    wip adding route && cors policy

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-  console.log('Request received!');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Thing created successfully!'
+  });
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Response sent successfully!');
+app.get('/api/stuff/1', (req, res, next) => {
+  console.log(req.params);
+  res.status(200).json({
+    message: 'Thing created successfully!'
+  });
 });
 
 module.exports = app;
