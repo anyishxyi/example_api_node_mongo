954d591069969cb57a0e4b83e1bc96119cf12f72
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-12-05 00:49:13 +0100

    add get and post route

--- app.js
+++ app.js
@@ -18,17 +18,42 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Thing created successfully!'
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
   });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 
-app.get('/api/stuff/1', (req, res, next) => {
-  console.log(req.params);
-  res.status(200).json({
-    message: 'Thing created successfully!'
-  });
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
 });
 
 module.exports = app;
