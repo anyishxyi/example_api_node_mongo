3129c4e730ee388ca85cb24b814fd73146b3fa89
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-12-07 21:19:32 +0100

    fix getamm

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Thing = require('./models/thing');
 
 const app = express();
 
