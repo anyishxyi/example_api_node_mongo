fb6146201ddaa17d008a003cb77d5f4731f86456
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-12-08 03:15:44 +0100

    wip from es5 to es6 app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'bodyParser';
 import mongoose from 'mongoose';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 const stuffRoutes = require('./routes/thing');
 const userRoutes = require('./routes/user');
@@ -11,14 +14,17 @@ mongoose.connect('mongodb+srv://admin:5ywvuz3M4YTuNrQt@cluster0.jf5ky.mongodb.ne
 				.then(() => { console.log('\nSuccessully connected to MongoDB Atlas !\n')})
 				.catch((error) => console.error('\nUnable to connect to MongoDB Atlas\n', error));
 
-app.use(bodyParser.json());
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
